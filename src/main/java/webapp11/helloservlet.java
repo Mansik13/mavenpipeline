48ed4f10898f9dfa08f26c6ff2cb0445fc3fa5ff
mansik <mansikargutkar548@gmail.com>
2021-09-06 20:36:34 +0530

    1st commit

---
+++ src/main/java/webapp11/helloservlet.java
@@ -0,0 +1,25 @@
+package webapp11;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+public class helloservlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public helloservlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Hello: ").append(request.getParameter("name"));
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
---
+++ target/webapp11/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=I50-8859-1" 
+pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>ENTER YOUR DETAILS</h2>
+
+<form action="helloservlet" method="get">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="Mansi"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Kargutkar"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
